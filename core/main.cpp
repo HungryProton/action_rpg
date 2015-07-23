faa038c900d5a0de457c7d7c13e2d841291797cc
Gilles RIVIERE <gilles.riviere@ansamb.com>
2015-07-23 19:46:57 +0200

    removed almost everything, starting from scratch to follow YAGNI principle

--- core/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <cstdlib>
-#include "core/game/game.hpp"
-
-int main(){
-    game::Game::Start();
-    return EXIT_SUCCESS;
-}
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,4 @@
 // Include all test suites 
-#include "core/test_game.hpp"
-#include "core/modules/render/test_render.hpp"
-#include "core/modules/simulation/test_simulation.hpp"
 #include "tools/test_logger.hpp"
 
 using namespace std;
@@ -9,8 +6,6 @@ using namespace std;
 int main( int argc, char * argv[] ){
 
     game::run_logger_test_suite(argc, argv);
-    game::run_render_test_suite(argc, argv);
-    game::run_simulation_test_suite(argc, argv);
 
     return EXIT_SUCCESS;
 }
