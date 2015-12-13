b5529bc7204cf77d6e9fac892facc7bd405ff032
Gilles RIVIERE <gilles.riviere@ansamb.com>
2015-12-13 18:29:22 +0100

    converted spaces to tab in gameobject.hpp

--- core/entity/gameobject/game_object.hpp
+++ core/entity/gameobject/game_object.hpp
@@ -8,92 +8,92 @@
 #include "core/messaging/message.hpp"
 namespace game{
 
-    class Game;
-    class System;
-    struct Component;
+	class Game;
+	class System;
+	struct Component;
 
-    class GameObject{
+	class GameObject{
 
-        public:
+		public:
 
-            /** \brief The base class of the game.
-             *
-             *  Every single game entity is a GameObject. No class should
-             *  inherit from this class. To create any Game Entity, create a
-             *  GameObject and attach different components on it to produce the
-             *  desired behavior.
-             */
-            GameObject();
-
-
-            /**
-             * The destructor detach properly every component attached to the
-             *  game object and destroys the object.
-             */
-            ~GameObject();
+			/** \brief The base class of the game.
+			 *
+			 *	Every single game entity is a GameObject. No class should
+			 *	inherit from this class. To create any Game Entity, create a
+			 *	GameObject and attach different components on it to produce the
+			 *	desired behavior.
+			 */
+			GameObject();
 
 
-            /** \brief Add a new component
-             *
-             *  \param type : The ID of the component to attach
-             *
-             *  Add a new component for the current
-             *  GameObject and update the components entries in the
-             *  game ComponentManager.
-             */
-            void AttachComponent(Component* c);
+			/**
+			 * The destructor detach properly every component attached to the
+			 *	game object and destroys the object.
+			 */
+			~GameObject();
 
 
-            void AttachSystem(System* c);
+			/** \brief Add a new component
+			 *
+			 *	\param type : The ID of the component to attach
+			 *
+			 *	Add a new component for the current
+			 *	GameObject and update the components entries in the
+			 *	game ComponentManager.
+			 */
+			void AttachComponent(Component* c);
+
+
+			void AttachSystem(System* c);
 
 
 						void Update();
 
 
-            /** \brief Notify every systems attached to the gameobject
-             *	that a new component has been attached and their current
+			/** \brief Notify every systems attached to the gameobject
+			 *	that a new component has been attached and their current
 						 *	target list might no longer be valid
-             */
+			 */
 						void NotifyAttachedSystems();
 
 
-            /**
-             * @brief Get the first matching component
-             *
-             * @return a pointer to the first matching component.
-             */
-            template< class T >
-            T* GetComponent();
+			/**
+			 * @brief Get the first matching component
+			 *
+			 * @return a pointer to the first matching component.
+			 */
+			template< class T >
+			T* GetComponent();
 
 
-            /** \brief Get all the matching components
-             *
-             *
-             * \return std::vector<T*> : Vector containing all
-             *      the components that matched the type you asked for
-             */
-            template<class T>
-            std::vector<T*> GetAllMatchingComponents();
+			/** \brief Get all the matching components
+			 *
+			 *
+			 * \return std::vector<T*> : Vector containing all
+			 *		the components that matched the type you asked for
+			 */
+			template<class T>
+			std::vector<T*> GetAllMatchingComponents();
 
 
 						std::vector<Component*> GetAllComponents();
 
 						GameObject* Clone();
-        private:
+		private:
 
-            /** \brief Stores pointers to the components attached
-             *  to the current GameObject
-             */
-            std::multimap<std::type_index, Component*> components_;
+			/** \brief Stores pointers to the components attached
+			 *	to the current GameObject
+			 */
+			std::multimap<std::type_index, Component*> components_;
 
-            std::vector<System*> systems_;
+			std::vector<System*> systems_;
 
 
-            /** \brief Pointers to the GameObject's
-             *  direct children
-             */
-            std::vector<GameObject*> children_;
-    };
+			/** \brief Pointers to the GameObject's
+			 *	direct children
+			 */
+			std::vector<GameObject*> children_;
+	};
 }
 
 #include "game_object.tcc"
